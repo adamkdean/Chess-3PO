e42ae2639317143ea323f7a3e81de2885f3b01ff
Imdsm <adamkdean@googlemail.com>
2014-01-08 12:54:54 +0000

    Added logging

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ var irc = require('irc'),
 
 var bot = new irc.Client(config.server, config.nick, config);
 
+console.log('configuring listeners...');
+
 bot.addListener('error', function (message) {
     console.error('ERROR: %s: %s', message.command, message.args.join(' '));
 });
 
 bot.addListener('message', function (from, to, message) {
+    console.log('%s => %s: %s', from, to, message);    
+
     if (to.match(/^[#&]/)) {
 
         if (message.match(/hello/i)) {

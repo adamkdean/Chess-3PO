e6580d4c7ce65f683fbd6e152c0a1395caee8af5
Imdsm <adamkdean@googlemail.com>
2014-01-08 13:27:00 +0000

    Testing ircee lib

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-var irc = require('irc'),
+/*var irc = require('irc'),
     config = require('./config');
 
 var bot = new irc.Client(config.server, config.nick, config);
@@ -25,5 +25,29 @@ bot.addListener('message', function (from, to, message) {
     }
 });
 
+console.log('end of script');*/
 
-console.log('end of script');
+var ircee = require('ircee'),
+    net = require('net');
+
+// Create a client
+var irc = ircee();
+
+irc.on('connect', function() {
+    // Load the core module. It keeps the connection alive 
+    // when loaded and provides the login method.    
+    var core = irc.use(require('ircee/core'));
+    // use the login method to send the nickname
+    core.login({
+        nick: 'ircee_example',
+        user: 'ircee_example',
+        name: 'Look! I am online!'
+    });
+});
+
+// Log all protocol lines to stdout
+irc.on('event', function(e) { console.log(e.raw); });
+
+// Connect the actual socket and pipe it to the client
+var s = net.connect(6667, 'irc.freenode.net');
+s.pipe(irc).pipe(s);

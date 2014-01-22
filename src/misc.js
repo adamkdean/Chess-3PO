e53431182e8d8acc205fd7196fbb7f24dd5013e6
Adam Dean <adam.dean@bravissimo.com>
2014-01-22 14:34:22 +0000

    Fixed it lols

--- src/misc.js
+++ src/misc.js
@@ -1,5 +1,5 @@
 var parse = function (bot, to, message) {    
-    if (message.match(/\b(you can do it)\b/i) {
+    if (message.match(/\b(you can do it)\b/i)) {
         bot.say(to, 'Yeah, you can do it');
     }
 };

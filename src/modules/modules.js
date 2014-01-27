4476b422c64488ab034aee398ed1bcf012cb65c1
Imdsm <adamkdean@googlemail.com>
2014-01-27 19:08:11 +0000

    Added !modules to show modules installed

---
+++ src/modules/modules.js
@@ -0,0 +1,21 @@
+var expression = /\!\b(modules)\b/i;
+
+var listModules = function (modules) {
+    var list = 'Modules installed: ';
+    for (var key in modules) {
+        list += key + ', ';
+    }
+    return list.substring(0, list.length - 2);
+};
+
+var parse = function (args) {
+    if (args.message.match(expression)) {
+        return listModules(args.modules);
+    }
+
+    return null;
+};
+
+module.exports = {
+    parse: parse
+};
